5e5fc1e836e955f11a314ff52962654810fe97f1
Eugene The Dream <ek@remote>
2025-10-27 10:38:33 -0700

    Fast indexer

--- tools/mirror/src/lib.rs
+++ tools/mirror/src/lib.rs
@@ -1774,6 +1774,7 @@ impl<T: ChainAccess> TxMirror<T> {
             await_for_node_synced: near_indexer::AwaitForNodeSyncedEnum::StreamWhileSyncing,
             finality: Finality::Final,
             validate_genesis: false,
+            interval: Duration::from_millis(500),
         })
         .context("failed to start target chain indexer")?;
         let (target_view_client, target_client, rpc_handler) = target_indexer.client_actors();
